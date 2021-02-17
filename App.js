aa684dca6cc0ed2b625172944c28e58382ad262c
ShashiniDileka <92947@fhss.sjp.ac.lk>
2021-02-17 11:22:07 +0530

    e

--- App.js
+++ App.js
@@ -1,4 +1,27 @@
-import React from 'react';
-import { NavigationContainer, StackActions } from  '@react-navigation/native';
+
+
+import 'react-native-gesture-handler';
+import React from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Welcome from './app/components/Welcome';
+
+
+const Stack=createStackNavigator();
+
+const App =()=>{
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+
+        <Stack.Screen
+           name="Welcome"
+           component={Welcome}
+    options={{title:"Welcome",headerShown:false}} /> 
+
+      </Stack.Navigator>
+       
+    </NavigationContainer>
+  )
+}
+
