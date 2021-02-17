f75ac1f80690fa33fb253ace7018ba3e748519e8
ShashiniDileka <92947@fhss.sjp.ac.lk>
2021-02-17 21:41:26 +0530

    screen development and navigation

---
+++ app/components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {StyleSheet,Text,View,ScrollView} from 'react-native';
+ 
+
+import Banner from './Banner';
+import title from './Topics';
+
+export default class Home extends React.Component{
+    render(){
+        return(
+            <ScrollView style= {StyleSheet.container}>
+                <Header/>
+                <Banner/>
+                <Topics/>
+
+            </ScrollView>
+        );
+    }
+}
+
+const styles =Stylesheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#fff',
+    }   
+});
---
+++ app/components/ProducDetails.js
@@ -0,0 +1,11 @@
+XPathExpression
+ddddddddddddddddd
+
+
+
+
+
+
+
+
+dddddd
